e19e25644859c464a20e5d629b50ba652afb8d77
Joelgs13 <050213jgonzalez@gmail.com>
2024-12-16 12:45:02 +0100

    Jasper Cargado

--- src/main/java/joel/dein/ejercicio1_jasper/HelloApplication.java
+++ src/main/java/joel/dein/ejercicio1_jasper/HelloApplication.java
@@ -1,51 +1,40 @@
 package joel.dein.ejercicio1_jasper;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import joel.dein.ejercicio1_jasper.BBDD.ConexionBBDD;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
+import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
-public class HelloApplication extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        // Ruta del archivo JRXML (puede ser compilado .jasper o .jrxml)
-        String reportPath = HelloApplication.class.getResource("/Coffee.jasper").getPath();
-        if (reportPath == null) {
-            System.out.println("El archivo Coffee.jrxml no se encontró.");
-        } else {
-            System.out.println("Ruta del archivo: " + reportPath);
-        }
-
-        // Crear los parámetros para el informe (si es necesario)
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("parametro1", "valor"); // Aquí puedes agregar parámetros si los necesitas
-
-        // Cargar el informe y llenar los datos
-        try {
-            // Cargar el informe
-            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(reportPath);
-
-            // Llenar el informe con los datos
-            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
-
-            // Mostrar el informe usando JasperViewer en un JFXPanel
-            // Esto abrirá una ventana que muestra el informe
-            JasperViewer.viewReport(jasperPrint, false);
-
-        } catch (JRException e) {
-            e.printStackTrace();
-        }
-    }
+public class HelloApplication{
 
     public static void main(String[] args) {
-        launch(args);
+        ConexionBBDD bbdd;
+        try {
+            bbdd = new ConexionBBDD();
+            InputStream reportStream = bbdd.getClass().getResourceAsStream("/JasperReport/Coffee.jasper");
+            if (reportStream == null) {
+                System.out.println("El archivo Coffee.jrxml no se encontró.");
+            } else {
+                System.out.println("Ruta del archivo: " + reportStream);
+            }
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(reportStream);
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("IMAGE_PATH", bbdd.getClass().getResource("/img/").toString());
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, ConexionBBDD.getConnection());
+            JasperViewer viewer = new JasperViewer(jasperPrint, false);
+            viewer.setVisible(true);
+        } catch (SQLException | JRException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
